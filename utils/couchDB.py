9b365eceaa2044251899fd8feaf1f5510c575b04
saiguptha2003 <saiguptha_v@srmap.edu.in>
2024-12-21 16:10:56 +0530

    updated couch services

--- utils/couchDB.py
+++ utils/couchDB.py
@@ -3,3 +3,11 @@ from constants import DBNAME
 
 couch = couchdb.Server('https://admin:iotstudio@couchdb-xfm8.onrender.com/')
 # cdb=couch[DBNAME]
+import json
+from couchdb.http import ResponseBody
+
+# Patch the decode method for ResponseBody if not already present
+if not hasattr(ResponseBody, 'decode'):
+    def decode(self):
+        return json.loads(self.read().decode('utf-8'))
+    setattr(ResponseBody, 'decode', decode)
