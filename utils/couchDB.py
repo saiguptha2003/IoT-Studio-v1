e8dfba01a1aead81df24524bdf92dd75d2e55966
saiguptha2003 <saiguptha_v@srmap.edu.in>
2024-12-21 13:42:58 +0530

    try couchdb connection

--- utils/couchDB.py
+++ utils/couchDB.py
@@ -3,4 +3,8 @@ import couchdb
 from constants import DBNAME
 couch = couchdb.Server('https://admin:iotstudio@couchdb-xfm8.onrender.com/')
 DBNAME='iotstudio'
+
 cdb=couch[DBNAME]
+if cdb is None:
+    cdb.create(DBNAME)
+    cdb.save()
