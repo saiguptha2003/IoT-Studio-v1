5daa9c819ccf60a0a12a9e819fbd62284217df44
saiguptha2003 <saiguptha_v@srmap.edu.in>
2024-12-21 13:46:15 +0530

    updated auto db creation couchdb

--- utils/couchDB.py
+++ utils/couchDB.py
@@ -1,10 +1,13 @@
 import couchdb
-
 from constants import DBNAME
 couch = couchdb.Server('https://admin:iotstudio@couchdb-xfm8.onrender.com/')
-DBNAME='iotstudio'
 
-cdb=couch[DBNAME]
-if cdb is None:
-    cdb.create(DBNAME)
-    cdb.save()
+if DBNAME not in couch:
+    print(f"Database '{DBNAME}' not found. Creating it...")
+    cdb = couch.create(DBNAME)
+else:
+    cdb = couch[DBNAME]
+    print(f"Connected to the database: {DBNAME}")
+doc = {'type': 'test', 'message': 'This is a test document.'}
+doc_id, doc_rev = cdb.save(doc)
+print(f"Document saved with ID: {doc_id}, Revision: {doc_rev}")
