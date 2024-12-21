e13bb1f7abf9577109c8d730c7e895529f1ca304
saiguptha2003 <saiguptha_v@srmap.edu.in>
2024-12-21 13:50:28 +0530

    updated couch services

--- services/couchdbServices.py
+++ services/couchdbServices.py
@@ -2,8 +2,20 @@ import json
 
 from flask import jsonify
 import couchdb
-from utils import cdb
+from constants import DBNAME
+from utils import couch
 def createDocumentForUser(userId: str,username :str, emailId: str):
+    cdb=None
+    if DBNAME not in couch:
+        print(f"Database '{DBNAME}' not found. Creating it...")
+        cdb = couch.create(DBNAME)
+    else:
+        cdb = couch[DBNAME]
+        print(f"Connected to the database: {DBNAME}")
+    doc = {'type': 'test', 'message': 'This is a test document.'}
+    doc_id, doc_rev = cdb.save(doc)
+    print(f"Document saved with ID: {doc_id}, Revision: {doc_rev}")
+
     doc={
         "_id":userId,
         "username":username,
