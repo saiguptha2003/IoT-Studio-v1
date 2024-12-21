3fc5280286d46a64fe8765221e61b509b8cf3f5e
saiguptha2003 <saiguptha_v@srmap.edu.in>
2024-12-21 13:52:28 +0530

    updated couch services

--- utils/couchDB.py
+++ utils/couchDB.py
@@ -1,13 +1,3 @@
 import couchdb
 from constants import DBNAME
 couch = couchdb.Server('https://admin:iotstudio@couchdb-xfm8.onrender.com/')
-
-if DBNAME not in couch:
-    print(f"Database '{DBNAME}' not found. Creating it...")
-    cdb = couch.create(DBNAME)
-else:
-    cdb = couch[DBNAME]
-    print(f"Connected to the database: {DBNAME}")
-doc = {'type': 'test', 'message': 'This is a test document.'}
-doc_id, doc_rev = cdb.save(doc)
-print(f"Document saved with ID: {doc_id}, Revision: {doc_rev}")
