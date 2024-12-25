f25796fe55164057f73380658f0965c08e264d17
saiguptha2003 <saiguptha_v@srmap.edu.in>
2024-12-25 22:31:27 +0530

    completed trigger functionality

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 from flask import Flask
 from models import db
 from Config import Config
-from routes import authBP, IoTConnectBP,SecureStoreBP,BasicBP
+from routes import authBP, IoTConnectBP,SecureStoreBP,BasicBP,TriggerBP
 from flask_cors import CORS
 import os
 import couchdb
@@ -21,7 +21,7 @@ app.register_blueprint(authBP, url_prefix='/auth')
 app.register_blueprint(IoTConnectBP, url_prefix='/services/IotConnect')
 app.register_blueprint(SecureStoreBP, url_prefix='/services/SecureStore')
 app.register_blueprint(BasicBP,url_prefix='/')
-
+app.register_blueprint(TriggerBP,url_prefix='/Trigger')
 @app.route("/",methods=["GET"])
 def index():
     return {"message": "hello User.."}
@@ -31,4 +31,4 @@ if __name__ == "__main__":
     if testing:
         print("Running in test mode")
     else:
-        app.run(host="0.0.0.0", port=FLASK_PORT)
+        app.run(host="0.0.0.0", port=FLASK_PORT,debug=True)
--- routes/__init__.py
+++ routes/__init__.py
@@ -2,3 +2,4 @@ from .auth import authBP
 from .IoTConnect import IoTConnectBP
 from .SecureStore import SecureStoreBP
 from .basic import BasicBP
+from .triggers import TriggerBP
