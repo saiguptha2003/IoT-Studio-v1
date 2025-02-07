7837b32ffda0a51eb066317fc89c495ecee17c07
pandu sai <saiguptha_v@srmap.edu.in>
2025-02-07 18:19:33 +0530

    added grpc files

--- IoT-Studio/utils/__init__.py
+++ IoT-Studio/utils/__init__.py
@@ -1,3 +1,5 @@
 from .utils import hashPassword, getUniqueID,checkPassword,getUniqueIDInt
 from .JWT_Token import create_token,decode_token,token_required
 from .couchDB import cdb
+from .iot_service_pb2 import ConnectionRequest,StopConnectionRequest
+from .iot_service_pb2_grpc import IoTServiceStub,IoTServiceStub
