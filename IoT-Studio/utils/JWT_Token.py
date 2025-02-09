6a00f5aa3899114d2982295c8af4c73560a6c061
pandu sai <saiguptha_v@srmap.edu.in>
2025-02-09 21:20:45 +0530

    formated

--- IoT-Studio/utils/JWT_Token.py
+++ IoT-Studio/utils/JWT_Token.py
@@ -13,9 +13,9 @@ def create_token(username,additional_claims=None,sessionRequired=180):
     }
     if additional_claims:
         payload.update(additional_claims)
-    
     token = jwt.encode(payload, SECRET_KEY, algorithm=JWT_ALGORITHM)
     return token
+
 
 def decode_token(token):
     """Decode a JWT token."""
@@ -27,22 +27,22 @@ def decode_token(token):
     except jwt.InvalidTokenError:
         return {"error": "Invalid token"}
 
+
 def token_required(f):
     """Decorator to protect routes that require authentication."""
     def wrapper(*args, **kwargs):
         token = request.headers.get("Authorization")
         if not token:
             return jsonify({"message": "Token is missing"}), 401
-
         token = token.split(" ")[1] if " " in token else token
         decoded = decode_token(token)
         if "error" in decoded:
             return jsonify({"message": decoded["error"]}), 401
-
         kwargs["userid"] = decoded.get("user_id")
         kwargs["email"] = decoded.get("email")
         kwargs['username'] = decoded.get("username")
         return f(*args, **kwargs)
+
 
     wrapper.__name__ = f.__name__
     return wrapper
--- IoT-Studio/utils/utils.py
+++ IoT-Studio/utils/utils.py
@@ -1,10 +1,18 @@
 import uuid
 from werkzeug.security import generate_password_hash, check_password_hash
+
+
 def hashPassword(password):
     return generate_password_hash(password)
+
+
 def checkPassword(password,password_hash):
     return check_password_hash(password_hash, password)
+
+
 def getUniqueID():
     return uuid.uuid4().hex
+
+
 def getUniqueIDInt():
     return uuid.uuid4().int
